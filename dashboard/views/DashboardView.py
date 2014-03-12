7fd00acdfd97dc3f2772417aa13179f8569c9a67
Jiuyue Ma <forever043@hotmail.com>
2014-03-12 22:12:42 +0800

    move DashboardView to "views/DashboardView.py"

--- dashboard/views/DashboardView.py
+++ dashboard/views/DashboardView.py
@@ -1,8 +1,8 @@
 # coding=utf-8
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
-from django.db.models import Count
 from django.views.generic import TemplateView
+from django.db.models import Count
 
 from libs.chartit import DataPool, Chart
 
--- ictpat/settings.py
+++ ictpat/settings.py
@@ -132,4 +132,5 @@ LOGOUT_URL = reverse('dashboard-logout')
 #for filebrowsers
 #FILEBROWSER_DIRECTORY = os.path.join(BASE_DIR, 'uploads')
 PATENT_FILE_DIRECTORY = os.path.join(BASE_DIR, 'patent_files')
+SCR_FILE_DIRECTORY = os.path.join(BASE_DIR, 'scr_files')
 
