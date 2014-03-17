ee2f57aa15db09cc614017d1fbd65fa992384e8b
Yifan Wang <wangyfan@outlook.com>
2014-03-17 22:43:20 +0800

    add query to retrvhome

--- retrvhome/urls.py
+++ retrvhome/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import RedirectView, TemplateView
 
-from retrvhome.models import Patent
+from retrvhome.models import Patent, PatentField
 from retrvhome.forms import RetrvFilterForm
 from retrvhome.views import *
 
@@ -10,12 +10,13 @@ urlpatterns = patterns('',
 	url(r'^$',			TemplateView.as_view(template_name='retrvhome/index.html'), name='retrvhome-index'),
 	url(r'^patent/$',
 		RetrvListView.as_view(
-				model = Patent,
+				model = PatentField,
 				form_class = RetrvFilterForm,
-				context_object_name = 'patent_list',
+				context_object_name = 'field_list',
 				template_name = 'retrvhome/patent_list.html',
 				success_url = reverse_lazy('patent-list')),
 		name='retrvhome-patent-list'),
+    url(r'^patent/data/$', 	PatentListJson.as_view(), name='retrvhome-patent-list-json'),
 	url(r'^scr/$',		TemplateView.as_view(template_name='retrvhome/index.html'), name='retrvhome-scr-list'),
 )
 
--- retrvhome/views.py
+++ retrvhome/views.py
@@ -1,16 +1,27 @@
 # -*- coding: UTF-8 -*-
 from django.http import HttpResponse
+from django.http import Http404
 from django.views.generic import ListView
 from django.views.generic.edit import FormMixin
 from django.contrib import messages
 from django.db import IntegrityError, DatabaseError
 from django.core import exceptions
+from django.db.models import Q
+from libs.django_datatables_view.base_datatable_view import BaseDatatableView
 
 from retrvhome.models import *
 from retrvhome.forms import *
 
 class RetrvListView(ListView, FormMixin):
-	paginate_by = 10
+	field_display_width = {
+		"name": 25,
+		"department": 13,
+		"inventors": 20,
+		"type": 8,
+		"apply_code": 10,
+		"apply_date": 8,
+		"authorize_code": 10,
+	}
 
 	def get_context_data(self, **kwargs):
 		context = super(RetrvListView, self).get_context_data(**kwargs)
@@ -43,18 +54,11 @@ class RetrvListView(ListView, FormMixin):
 		return initial
 
 	def get_queryset(self):
-		query_filter_list = self.request.session.get('query-filter', {})
-		messages.warning(self.request, query_filter_list)
-		object_filter = {}
-		for (filter_name, filter_value) in query_filter_list.items():
-                        if 'pk' in filter_value:
-                                object_filter[filter_name] = filter_value['pk']
-                        elif 'str' in filter_value:
-                                object_filter[filter_name + '__icontains'] = filter_value['str']
-		object_list = self.model.objects.filter(**object_filter)
+		object_list = self.model.objects.filter(display=True).order_by('sort')
 		if not object_list:
-			messages.warning(self.request, "没有找到符合条件的专利")
-
+			raise Http404
+		for o in object_list:
+			o.width = self.field_display_width.get(o.field_name, None)
 		return object_list
 
 	def form_valid(self, form):
@@ -72,4 +76,45 @@ class RetrvListView(ListView, FormMixin):
 	def form_invalid(self, form):
 		return super(RetrvListView, self).form_invalid(form)
 
+
+class PatentListJson(BaseDatatableView):
+	model = Patent
+	field_model = PatentField
+	columns = []
+	#columns = ['department', 'name', 'inventors', 'apply_code', 'apply_date', 'authorize_code', 'pk', 'DT_RowId']
+	#order_columns = ['field_label', 'field_name', '', '', 'sort', 'pk', 'pk']
+	#max_display_length = 2000
+
+	def __init__(self, *args, **kwargs):
+		super(PatentListJson, self).__init__(*args, **kwargs)
+		if not self.columns:
+			self.columns = [ 'pk', 'DT_RowId' ]
+			field_list = self.field_model.objects.filter(display=True).order_by('sort')
+			if not field_list:
+				raise Http404
+			for field in field_list:
+				self.columns.append(field.field_name)
+
+	def filter_queryset(self, qs):
+		sSearch = self.request.GET.get('sSearch', None)
+		if sSearch:
+			qs = qs.filter(Q(name__icontains=sSearch))
+		department = self.request.GET.get('department', None)
+		if department:
+			qs = qs.filter(Q(department__icontains=department))
+		return qs
+
+	def render_column(self, row, column):
+		if column == 'DT_RowId':
+			return '%d' % (row.pk)
+		elif column == 'name':
+			return u'<a href="#">%s</a>' % row.name
+		elif column == 'apply_code':
+			return u'<a href="#">%s</a>' % row.apply_code
+		elif column == 'authorize_code':
+			if row.authorize_code:
+				return u'<a href="#">%s</a>' % row.authorize_code
+			else:
+				return row.state
+		return super(PatentListJson, self).render_column(row, column)
 
