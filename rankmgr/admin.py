f0abf591e70a1bee32544918eb77ebf6e6a6fc6e
Jiuyue Ma <forever043@hotmail.com>
2014-07-01 22:19:30 +0800

    add ExpertProfile; re-impl rating detail UI use raty;

--- rankmgr/admin.py
+++ rankmgr/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from rankmgr.models import *
 
 # Register your models here.
+class ExpertProfileAdmin(admin.ModelAdmin):
+	list_display = ('pk', 'phone', 'organization', 'research_field')
+	choise_display = 'pk'
 class PatentPackageAdmin(admin.ModelAdmin):
 	list_display = ('name', 'submit_date', 'finish_date')
 	choise_display = 'name'
@@ -11,6 +14,7 @@ class PatentRatingReportAdmin(admin.ModelAdmin):
 class PatentExpertRatingAdmin(admin.ModelAdmin):
 	list_display = ('package', 'patent', 'expert', 'rank', 'remark', 'submit_date')
 	choise_display = 'expert'
+admin.site.register(ExpertProfile, ExpertProfileAdmin)
 admin.site.register(PatentPackage, PatentPackageAdmin)
 admin.site.register(PatentRatingReport, PatentRatingReportAdmin)
 admin.site.register(PatentExpertRating, PatentExpertRatingAdmin)
