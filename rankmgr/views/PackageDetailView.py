9e1f9b819e23ca0c5c537dcaffcf379bb19a5199
Yifan Wang <wangyfan@outlook.com>
2014-07-06 11:23:51 +0800

    addon PatentPackageDetail & RatingDetail

--- rankmgr/views/PackageDetailView.py
+++ rankmgr/views/PackageDetailView.py
@@ -27,11 +27,15 @@ class PatentPackageDetailView(DetailView):
 				context['i__next__'] = self.default_referer_url
 		# Fill extra attributes
 		self.object.expert_list   = {}.fromkeys([o.expert for o in PatentExpertRating.objects.filter(patent__package = self.object).order_by('expert')]).keys()
+		for o in self.object.expert_list:
+			o.profile = o.get_profile()
 		self.object.patent_count  = PatentRatingReport.objects.filter(package=self.object).count()
 		self.object.rating_finish = PatentExpertRating.objects.filter(package=self.object).filter(submit_date__gt="1900-01-01").count()
  		self.object.rating_total  = PatentExpertRating.objects.filter(package=self.object).count()
+		self.object.rating_percent = 100*self.object.rating_finish/self.object.rating_total
 		self.object.report_finish = PatentRatingReport.objects.filter(package=self.object).filter(finish_date__gt="1900-01-01").count()
 		self.object.report_total  = PatentRatingReport.objects.filter(package=self.object).count()
+		self.object.report_percent = 100*self.object.report_finish / self.object.report_total
 		# Prepare patent list
 		rating_list = PatentExpertRating.objects.filter(patent__package=self.object).order_by("patent", "expert")
 		for o in rating_list:
